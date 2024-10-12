8539326d783d0c4280e1d1b7dfd94e503f8f5bb6
KangMyeongH <3044965@naver.com>
2024-10-12 14:25:06 +0900

    Transform 클래스 개선
    더티 플래그 제거
    부모의 Transform의 변경되면
    바로바로 자식의 Trasnsform을 값을 업데이트 해줌

--- APIProject/Transform.h
+++ APIProject/Transform.h
@@ -6,100 +6,94 @@ class GameObject;
 
 class Transform final : public Component
 {
-	// 해당 Transform 컴포넌트는 더티 플래그와 캐싱을 사용해서 월드 변환을 최적화 시켜주고 있습니다.
-	// 로컬 변환이 변경되었거나 부모가 변경된 경우에만 플래그를 true로 설정하고, 그때만 월드 변환을 다시 계산합니다.
 public:
 	explicit Transform(GameObject* owner)
 		: Component(owner), mLocalPosition{ 0,0 }, mLocalRotation(0), mLocalScale{ 1.f, 1.f }, mWorldPosition{ 0.f,0.f },
-		mWorldRotation(0), mWorldScale(1.f, 1.f), mParent(nullptr), mIsDirty(true) {}
+		mWorldRotation(0), mWorldScale(1.f, 1.f), mParent(nullptr) {}
 
 	// Local Position getter and setter
 	Vector2 GetLocalPosition() const { return mLocalPosition; }
 	void SetLocalPosition(const Vector2& position)
 	{
 		mLocalPosition = position;
-		MarkDirty();
+		UpdateWorldPosition();
+		UpdateChildrenWorldPosition();
 	}
 
 	// World Position getter
-	Vector2 GetWorldPosition()
-	{
-		if (mIsDirty)
-		{
-			UpdateWorldTransform();
-		}
-		return mWorldPosition;
-	}
-
+	Vector2 GetWorldPosition() const { return mWorldPosition; }
 	void SetWorldPosition(const Vector2& position)
 	{
 		if (mParent)
 		{
-			mLocalPosition = position - mParent->GetWorldPosition();
+			SetLocalPosition(position - mParent->GetWorldPosition());
 		}
+
 		else
 		{
-			mLocalPosition = position;
+			SetLocalPosition(position);
 		}
-		MarkDirty();
 	}
 
 	// Local Rotation getter and setter
 	float GetLocalRotation() const { return mLocalRotation; }
-	void SetLocalRotation(float rotation)
+	void SetLocalRotation(const float rotation)
 	{
 		mLocalRotation = rotation;
-		MarkDirty();
+		UpdateWorldRotation();
+		UpdateChildrenWorldRotation();
 	}
 
 	// World Rotation getter
-	float GetWorldRotation()
-	{
-		if (mIsDirty)
-		{
-			UpdateWorldTransform();
-		}
-		return mWorldRotation;
-	}
+	float GetWorldRotation() const { return mWorldRotation; }
 
 	// Local Scale getter and setter
 	Vector2 GetLocalScale() const { return mLocalScale; }
 	void SetLocalScale(const Vector2& scale)
 	{
 		mLocalScale = scale;
-		MarkDirty();
+		UpdateWorldScale();
+		UpdateChildrenWorldScale();
 	}
 
 	// World Scale getter
-	Vector2 GetWorldScale()
-	{
-		if (mIsDirty)
-		{
-			UpdateWorldTransform();
-		}
-		return mWorldScale;
-	}
+	Vector2 GetWorldScale() const { return mWorldScale; }
 
 	// Local position manipulation
 	void Translate(const Vector2& delta)
 	{
-		mLocalPosition += delta;
-		MarkDirty();
+		if (delta.x != 0.f || delta.y != 0.f)
+		{
+			mLocalPosition += delta;
+			UpdateWorldPosition();
+			UpdateChildrenWorldPosition();
+		}
 	}
 
 	// Transform hierarchy management
 	void SetParent(Transform* parent)
 	{
-		if (mParent != nullptr)
+		if (mParent != parent)
 		{
-			mParent->RemoveChild(this);
+			if (mParent != nullptr)
+			{
+				mParent->RemoveChild(this);
+			}
+
+			mParent = parent;
+
+			if (mParent != nullptr)
+			{
+				mParent->AddChild(this);
+			}
+
+			else
+			{
+				// Clear parent
+				UpdateWorldTransform();
+				UpdateChildrenTransforms();
+			}
 		}
-		mParent = parent;
-		if (mParent != nullptr)
-		{
-			mParent->AddChild(this);
-		}
-		MarkDirty();
 	}
 
 	Transform* GetParent() const { return mParent; }
@@ -114,32 +108,83 @@ private:
 		mChildren.erase(std::remove(mChildren.begin(), mChildren.end(), child), mChildren.end());
 	}
 
-	void MarkDirty()
-	{
-		mIsDirty = true;
-		for (Transform* child : mChildren)
-		{
-			child->MarkDirty();
-		}
-	}
-
-	void UpdateWorldTransform()
+	void UpdateWorldPosition()
 	{
 		if (mParent)
 		{
 			mWorldPosition = mParent->GetLocalPosition() + mLocalPosition;
-			mWorldRotation = mParent->GetLocalRotation() + mLocalRotation;
-			mWorldScale = Vector2(mLocalScale.x * mParent->GetWorldScale().x, mLocalScale.y * mParent->GetWorldScale().y);
 		}
 
 		else
 		{
 			mWorldPosition = mLocalPosition;
+		}
+	}
+
+	void UpdateWorldRotation()
+	{
+		if (mParent)
+		{
+			mWorldRotation = mParent->GetWorldRotation() + mLocalRotation;
+		}
+		else
+		{
 			mWorldRotation = mLocalRotation;
-			mWorldScale = mLocalScale;
+		}
+	}
+
+	void UpdateWorldScale()
+	{
+		if (mParent)
+		{
+			mWorldScale = Vector2(mLocalScale.x * mParent->GetWorldScale().x, mLocalScale.y * mParent->GetWorldScale().y);
 		}
 
-		mIsDirty = false;
+		else
+		{
+			mWorldScale = mLocalScale;
+		}
+	}
+
+	void UpdateChildrenWorldPosition()
+	{
+		for (Transform* child : mChildren)
+		{
+			child->UpdateWorldPosition();
+			child->UpdateChildrenWorldPosition();
+		}
+	}
+
+	void UpdateChildrenWorldRotation()
+	{
+		for (Transform* child : mChildren)
+		{
+			child->UpdateWorldRotation();
+			child->UpdateChildrenWorldRotation();
+		}
+	}
+
+	void UpdateChildrenWorldScale()
+	{
+		for (Transform* child : mChildren)
+		{
+			child->UpdateWorldScale();
+			child->UpdateChildrenWorldScale();
+		}
+	}
+
+	void UpdateWorldTransform()
+	{
+		UpdateWorldPosition();
+		UpdateWorldRotation();
+		UpdateWorldScale();
+	}
+
+	void UpdateChildrenTransforms()
+	{
+		UpdateChildrenWorldPosition();
+		UpdateChildrenWorldRotation();
+		UpdateChildrenWorldScale();
 	}
 
 private:
@@ -153,6 +198,4 @@ private:
 
 	Transform* 					mParent;
 	std::vector<Transform*> 	mChildren;
-
-	bool 						mIsDirty;
 };
